10a975b172cdc3b562b9860a10ea5de72fd82dc2
yashvikaushik <yashvikaushik2525@gmail.com>
2025-08-27 12:27:22 +0530

    pointer with character array

---
+++ DSA--Playground/pointers/CharArray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char ch[10]="abcdef";
+    char *ptr=&ch[0];
+    cout<<*ptr<<endl;//gives the values dtored at first memory block
+    cout<<ptr<<endl;//gives the whole array content
+    cout<<ch<<endl;//gives the whole array content
+    cout<<ch[0]<<endl;//gives value at index 0
+
+    char temp='z';
+    char *p=&temp;
+    cout<<temp<<endl;//gives z
+    cout<<*p<<endl;//gives z
+    cout<<p<<endl;//starts printing from z and continuing till it finds a null character
+    
+    return 0;
+}
