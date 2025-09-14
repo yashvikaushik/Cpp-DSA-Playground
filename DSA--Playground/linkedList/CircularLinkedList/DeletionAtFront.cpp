44ac23f15058f7faf05e2f7e0493f405f1de4033
yashvikaushik <yashvikaushik2525@gmail.com>
2025-09-14 15:29:02 +0530

    deletion

---
+++ DSA--Playground/linkedList/CircularLinkedList/DeletionAtFront.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+struct Node{
+int data;
+Node *next;
+};
+
+class CircularLinkedList{
+    Node *cursor;
+    public:
+    CircularLinkedList(){
+        cursor=NULL;
+    }
+    CircularLinkedList(int x){
+        cursor=new Node;
+        cursor->data=x;
+        cursor->next=cursor;
+    }
+
+    void InsertAtFront(int value){
+        Node *newNode=new Node;
+        newNode->data=value;
+        newNode->next=NULL;
+        if(cursor==NULL){
+            cursor=newNode;
+            cursor->next=cursor;
+            return;
+
+        }
+        newNode->next=cursor->next;
+        cursor->next=newNode;
+
+
+    }
+
+    void deleteFirst(){
+        Node *nodeToDel;
+        if(cursor==NULL){
+            cout<<"Nothing to delete"<<endl;
+            return;
+        }
+        if(cursor==cursor->next){
+            delete cursor;
+            cursor=NULL;
+            return;
+        }
+
+        nodeToDel=cursor->next;
+        cursor->next=nodeToDel->next;
+        delete nodeToDel;
+    }
+
+    void print(){
+        Node *temp=cursor->next;
+        do{
+            cout<<temp->data<<" -> ";
+            temp=temp->next;
+
+
+
+        }
+        while(temp!=cursor->next);
+    }
+};
+int main(){
+    CircularLinkedList node1;
+    node1.InsertAtFront(10);
+    node1.InsertAtFront(20);
+    node1.InsertAtFront(30);
+    node1.InsertAtFront(40);
+    node1.deleteFirst();
+    node1.print();
+    return 0;
+}
