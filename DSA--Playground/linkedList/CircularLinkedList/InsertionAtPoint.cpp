a7205c46aac1aef609189f72ea55196490d7c8e1
yashvikaushik <yashvikaushik2525@gmail.com>
2025-09-14 15:29:30 +0530

    insertion

---
+++ DSA--Playground/linkedList/CircularLinkedList/InsertionAtPoint.cpp
@@ -0,0 +1,99 @@
+ #include <iostream>
+using namespace std;
+struct Node{
+int data;
+Node *next;
+};
+
+class CircularLinkedList{
+    Node *cursor;
+    public:
+    CircularLinkedList(){
+        cursor=NULL;
+    }
+    CircularLinkedList(int x){
+        cursor=new Node;
+        cursor->data=x;
+        cursor->next=cursor;
+    }
+
+    void InsertAtPoint(int y,int value){
+        Node *newNode=new Node;
+        newNode->data=value;
+        newNode->next=NULL;
+        if(cursor==NULL){
+            cursor=newNode;
+            cursor->next=cursor;
+            return;
+
+        }
+        if(cursor->data==y){
+            newNode->next=cursor->next;
+            cursor->next=newNode;
+            cursor=newNode;
+            return;
+
+
+        }
+
+        Node *temp=cursor->next;
+        do{
+            if(temp->data==y){
+                break;
+            }
+            temp=temp->next;
+
+        }
+        while(temp!=cursor->next);
+
+        if(temp==cursor->next && temp->data!=y){
+        cout<<"not found"<<endl;
+        delete newNode;   // avoid memory leak
+        return;
+    }
+
+        newNode->next=temp->next;
+        temp->next=newNode;
+        
+
+
+    }
+
+    void InsertAtEnd(int value){
+        Node *newNode=new Node;
+        newNode->data=value;
+        newNode->next=NULL;
+        if(cursor==NULL){
+            cursor=newNode;
+            cursor->next=cursor;
+            return;
+
+        }
+        newNode->next=cursor->next;
+        cursor->next=newNode;
+        cursor=newNode;
+
+
+    }
+
+    void print(){
+        Node *temp=cursor->next;
+        do{
+            cout<<temp->data<<" -> ";
+            temp=temp->next;
+
+
+
+        }
+        while(temp!=cursor->next);
+    }
+};
+int main(){
+    CircularLinkedList node1;
+    node1.InsertAtEnd(8);
+    node1.InsertAtEnd(2);
+    node1.InsertAtPoint(10,14);
+
+    node1.print();
+    return 0;
+}
---
+++ DSA--Playground/linkedList/CircularLinkedList/insertAtFront.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+struct Node{
+int data;
+Node *next;
+};
+
+class CircularLinkedList{
+    Node *cursor;
+    public:
+    CircularLinkedList(){
+        cursor=NULL;
+    }
+    CircularLinkedList(int x){
+        cursor=new Node;
+        cursor->data=x;
+        cursor->next=cursor;
+    }
+
+    void InsertAtFront(int value){
+        Node *newNode=new Node;
+        newNode->data=value;
+        newNode->next=NULL;
+        if(cursor==NULL){
+            cursor=newNode;
+            cursor->next=cursor;
+            return;
+
+        }
+        newNode->next=cursor->next;
+        cursor->next=newNode;
+
+
+    }
+
+    void print(){
+        Node *temp=cursor->next;
+        do{
+            cout<<temp->data<<" -> ";
+            temp=temp->next;
+
+
+
+        }
+        while(temp!=cursor->next);
+    }
+};
+int main(){
+    CircularLinkedList node1;
+    node1.InsertAtFront(8);
+    node1.InsertAtFront(2);
+    node1.print();
+    return 0;
+}
